ca2934891bb4ce7566dbfb26abfbc64b8079b13f
Ilya <gashimovilya2018@gmail.com>
2025-03-28 17:16:15 +0200

    fix(category): add missing stream relation in findAll and findRandom methods

--- backend/src/modules/category/category.module.ts
+++ backend/src/modules/category/category.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import {CategoryResolver} from "@/src/modules/category/category.resolver";
 
 @Module({
-  providers: [CategoryService],
+  providers: [CategoryService, CategoryResolver],
 })
 export class CategoryModule {}
--- backend/src/modules/category/category.service.ts
+++ backend/src/modules/category/category.service.ts
@@ -11,6 +11,14 @@ export class CategoryService {
         const categories = await this.prismaService.category.findMany({
             orderBy: {
                 createdAt: 'desc'
+            },
+            include: {
+                streams: {
+                    include: {
+                        user: true,
+                        category: true
+                    }
+                }
             }
         })
 
@@ -29,6 +37,14 @@ export class CategoryService {
         }
 
         const categories = await this.prismaService.category.findMany({
+            include: {
+                streams: {
+                    include: {
+                        user: true,
+                        category: true
+                    }
+                }
+            },
             take: total,
             skip: 0,
         })
