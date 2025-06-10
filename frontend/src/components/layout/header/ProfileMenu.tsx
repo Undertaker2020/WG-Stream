5b980a4305ce7c82f50423acd2c1e5dcfa25739f
Ilya <gashimovilya2018@gmail.com>
2025-06-10 19:00:58 +0300

    fix(clear session cookie)

--- frontend/src/components/layout/header/ProfileMenu.tsx
+++ frontend/src/components/layout/header/ProfileMenu.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/common/DropdownMenu";
 import {ChannelAvatar} from "@/components/ui/elements/ChannelAvatar";
 import Link from "next/link";
-import {useLogoutUserMutation} from "@/graphql/generated/output";
+import {useClearSessionCookieMutation, useLogoutUserMutation} from "@/graphql/generated/output";
 import {toast} from "sonner";
 import {Notifications} from "@/components/layout/header/notifications/Notifications";
 
@@ -24,8 +24,10 @@ export function ProfileMenu() {
 
     const { exit } = useAuth();
     const {user, isLoadingProfile} = useCurrent()
+    const [clear] = useClearSessionCookieMutation();
     const [logout] = useLogoutUserMutation({
         onCompleted(){
+            clear()
             exit()
             toast.success(t('successMessage'))
             router.push('/account/login')
